e76942ea9dc5a8ff4269bbe6efa7e23181291416
miraDask <mira.daskalov@gmail.com>
2019-09-18 13:21:28 +0100

    adding problem 8

---
+++ 01.DATA DEFINITION AND DATATYPES/P08.Create Table Users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Users(
+	Id BIGINT PRIMARY KEY IDENTITY,
+	Username VARCHAR(30) UNIQUE NOT NULL,
+	[Password] VARCHAR(26) NOT NULL,
+	ProfilePicture VARBINARY(MAX),
+	CONSTRAINT CHK_ProfilePictureSizeIsUpTo900KB CHECK(DATALENGTH(ProfilePicture)<= 921600),
+	LastLoginTime DATETIME2,
+	IsDeleted BIT NOT NULL
+)
+
+INSERT INTO Users (Username, [Password], ProfilePicture, LastLoginTime, IsDeleted) VALUES
+('Gogo', 'pass123', NULL, NULL, 0),
+('Gogogo', 'pass123', NULL, NULL, 1),
+('Gogogogo', 'pass123', NULL, NULL, 1),
+('Gogogogogo', 'pass123', NULL, NULL, 0),
+('Go', 'pass123', NULL, NULL, 0)
