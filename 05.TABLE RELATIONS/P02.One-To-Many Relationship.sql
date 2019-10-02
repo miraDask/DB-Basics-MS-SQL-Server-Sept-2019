ac3b704601370b1443bfe004e79faf5370c6f9c3
miraDask <mira.daskalov@gmail.com>
2019-10-02 16:15:05 +0100

    adding P02.One-To-Many Relationship.sql

---
+++ 05.TABLE RELATIONS/P02.One-To-Many Relationship.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Manufacturers(
+			 ManufacturerID INT PRIMARY KEY IDENTITY(1, 1),
+			 [Name] VARCHAR(30) NOT NULL,
+			 EstablishedOn DATE NOT NULL 
+)
+
+INSERT INTO Manufacturers([Name], EstablishedOn) VALUES
+            ('BMW', '07/03/1916'),
+            ('Tesla', '01/01/2003'),
+            ('Lada', '01/05/1966')
+
+
+CREATE TABLE Models(
+			 ModelID INT PRIMARY KEY IDENTITY(101, 1),
+			 [Name] VARCHAR(30) NOT NULL,
+			 ManufacturerID INT NOT NULL
+
+			 CONSTRAINT FK_ModelsManifacturerId
+             FOREIGN KEY (ManufacturerID)
+             REFERENCES Manufacturers (ManufacturerID)
+)
+
+
+INSERT INTO Models([Name], ManufacturerID) VALUES
+            ('X1', 1),
+            ('i6', 1),
+            ('Model S', 2),
+            ('Model X', 2),
+            ('Model 3', 2),
+            ('Nova', 3)
+
