02a947994fd186ec44d06c064ece3c061bb19328
miraDask <mira.daskalov@gmail.com>
2019-10-03 10:26:42 +0100

    adding P04.Self-Referencing.sql

---
+++ 05.TABLE RELATIONS/P05.Online Store Database.sql
@@ -0,0 +1,46 @@
+--CREATE DATABASE OnlineStore 
+
+CREATE TABLE Cities(
+			 CityID INT PRIMARY KEY IDENTITY,
+			 [Name] VARCHAR(50) NOT NULL
+)
+
+---
+
+CREATE TABLE Customers(
+			 CustomerID INT PRIMARY KEY IDENTITY,
+			 [Name] VARCHAR(50) NOT NULL,
+			 Birthday DATE, 
+			 CityID INT FOREIGN KEY REFERENCES Cities(CityID)
+)
+
+---
+
+CREATE TABLE Orders(
+			 OrderID INT PRIMARY KEY IDENTITY,
+			 CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID)
+)
+
+---
+
+CREATE TABLE ItemTypes(
+			 ItemTypeID INT PRIMARY KEY IDENTITY,
+			 [Name] VARCHAR(50) NOT NULL
+)
+
+---
+
+CREATE TABLE Items(
+			 ItemID INT PRIMARY KEY IDENTITY,
+			 [Name] VARCHAR(50) NOT NULL,
+			 ItemTypeID INT NOT NULL FOREIGN KEY REFERENCES ItemTypes(ItemTypeID)
+)
+
+---
+
+CREATE TABLE OrderItems(
+			 OrderID INT NOT NULL FOREIGN KEY REFERENCES Orders(OrderID),
+			 ItemID INT NOT NULL FOREIGN KEY REFERENCES Items(ItemID)
+
+			 CONSTRAINT PK_OrderItems PRIMARY KEY (OrderID, ItemID)  
+)
