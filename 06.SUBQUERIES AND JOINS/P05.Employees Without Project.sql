a1b97b36a57a8d032728ca586f4d035cd8f2c42b
miraDask <mira.daskalov@gmail.com>
2019-10-04 19:18:09 +0100

    adding P05.Employees Without Project.sql

---
+++ 06.SUBQUERIES AND JOINS/P05.Employees Without Project.sql
@@ -0,0 +1,8 @@
+    SELECT TOP(3)
+	       e.EmployeeID,
+		   e.FirstName
+	  FROM Employees AS e
+	  LEFT JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+	  LEFT JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+	 WHERE ep.EmployeeID IS NULL
+  ORDER BY e.EmployeeID
