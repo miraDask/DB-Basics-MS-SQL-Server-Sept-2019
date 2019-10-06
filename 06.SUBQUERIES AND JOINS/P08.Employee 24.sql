5205b514902bf38b979401b8fcc9df14d821cd3f
miraDask <mira.daskalov@gmail.com>
2019-10-06 10:56:10 +0100

    refactoring

--- 06.SUBQUERIES AND JOINS/P08.Employee 24.sql
+++ 06.SUBQUERIES AND JOINS/P08.Employee 24.sql
@@ -1,10 +1,6 @@
    SELECT e.EmployeeID,
           e.FirstName,
-		  CASE 
-			WHEN p.StartDate >= '2005-01-01'
-			THEN NULL
-			ELSE p.[Name]
-		  END AS ProjectName
+		  IIF(p.StartDate >= '2005-01-01', NULL,p.[Name]) AS ProjectName
      FROM Employees AS e
 	 JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
 	 JOIN Projects AS p ON ep.ProjectID = p.ProjectID
