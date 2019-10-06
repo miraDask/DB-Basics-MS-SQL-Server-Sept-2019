66ad915ed79ad647659af3eff93594cea6113278
miraDask <mira.daskalov@gmail.com>
2019-10-06 12:59:34 +0100

    refactoring

--- 06.SUBQUERIES AND JOINS/P17.Highest Peak and Longest River by Country.sql
+++ 06.SUBQUERIES AND JOINS/P17.Highest Peak and Longest River by Country.sql
@@ -3,10 +3,10 @@
 	       MAX(p.Elevation) AS HighestPeakElevation,
 		   MAX(r.[Length]) AS LongestRiverLength 
 	  FROM Countries AS c
-	  FULL JOIN CountriesRivers AS cr ON cr.CountryCode = c.CountryCode
-	  FULL JOIN Rivers AS r ON r.Id = cr.RiverId
-	  FULL JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
-	  FULL JOIN Mountains AS m oN m.Id = mc.MountainId
-	  FULL JOIN Peaks AS p ON p.MountainId = m.Id
+ LEFT JOIN CountriesRivers AS cr ON cr.CountryCode = c.CountryCode
+ LEFT JOIN Rivers AS r ON r.Id = cr.RiverId
+ LEFT JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
+ LEFT JOIN Mountains AS m oN m.Id = mc.MountainId
+ LEFT JOIN Peaks AS p ON p.MountainId = m.Id
      GROUP BY c.CountryName
      ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, c.CountryName
