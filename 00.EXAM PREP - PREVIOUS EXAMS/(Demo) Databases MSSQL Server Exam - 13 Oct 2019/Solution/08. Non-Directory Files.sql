e27c87713013ad4e9423967863ab29fe14fd4a6c
miraDask <mira.daskalov@gmail.com>
2019-10-16 14:23:18 +0100

    adding Solution/08. Non-Directory Files.sql

---
+++ 00.EXAM PREP - PREVIOUS EXAMS/(Demo) Databases MSSQL Server Exam - 13 Oct 2019/Solution/08. Non-Directory Files.sql
@@ -0,0 +1,9 @@
+	SELECT Id,
+	       [Name],
+		   CONCAT(Size, 'KB') AS Size
+	  FROM Files
+	 WHERE Id NOT IN 
+	(SELECT ParentId 
+      FROM Files
+	 WHERE ParentId IS NOT NULL )
+  ORDER BY Id, [Name], Size
