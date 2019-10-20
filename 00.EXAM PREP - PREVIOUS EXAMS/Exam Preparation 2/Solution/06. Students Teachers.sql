9b09fe86bf450fdc856ae69e128b863117c6b649
miraDask <mira.daskalov@gmail.com>
2019-10-20 18:41:43 +0100

    adding Solution/06. Students Teachers.sql

---
+++ 00.EXAM PREP - PREVIOUS EXAMS/Exam Preparation 2/Solution/06. Students Teachers.sql
@@ -0,0 +1,6 @@
+   SELECT s.FirstName,
+       	  s.LastName,
+		  COUNT(st.TeacherId) AS TeachersCount 
+     FROM Students AS s
+     JOIN StudentsTeachers AS st ON st.StudentId = s.Id
+ GROUP BY FirstName, LastName
