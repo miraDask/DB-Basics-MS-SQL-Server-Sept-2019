5028e5bff8d6e2411938fae9df2b65d679f1e2c9
miraDask <mira.daskalov@gmail.com>
2019-11-05 16:53:36 +0000

    refactoring Solution/09. Users per Employee.sql

--- 00.EXAM PREP - PREVIOUS EXAMS/Databases MSSQL Server Exam - 20 Oct 2019/Solution/09. Users per Employee.sql
+++ 00.EXAM PREP - PREVIOUS EXAMS/Databases MSSQL Server Exam - 20 Oct 2019/Solution/09. Users per Employee.sql
@@ -1,7 +1,7 @@
    SELECT CONCAT(e.FirstName, ' ', e.LastName) AS FullName,
           COUNT(r.UserId) AS UsersCount
      FROM Employees AS e
-	 JOIN Reports AS r ON r.EmployeeId = e.Id
-	 JOIN Users AS u ON u.Id = r.UserId
+LEFT JOIN Reports AS r ON r.EmployeeId = e.Id
+LEFT JOIN Users AS u ON u.Id = r.UserId
  GROUP BY e.FirstName, e.LastName
  ORDER BY UsersCount DESC, FullName 
