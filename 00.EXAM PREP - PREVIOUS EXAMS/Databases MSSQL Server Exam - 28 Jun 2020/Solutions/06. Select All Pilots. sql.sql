f77b4bf2db710ea5383e7134ff7dec91d7a70dfc
miraDask <mira.daskalov@gmail.com>
2020-08-09 18:43:27 +0100

    add solution for problem 6

---
+++ 00.EXAM PREP - PREVIOUS EXAMS/Databases MSSQL Server Exam - 28 Jun 2020/Solutions/06. Select All Pilots. sql.sql
@@ -0,0 +1,6 @@
+SELECT c.Id,
+	   c.FirstName + ' ' + c.LastName AS full_name
+  FROM Colonists AS c
+  JOIN TravelCards AS t ON t.ColonistId = c.Id
+ WHERE t.JobDuringJourney = 'Pilot'
+ ORDER BY C.Id
